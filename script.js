111f07c320ec36b8f3c55d84783b15f2382e190a
NataliSDesing <sudenko.natasha123@gmail.com>
2025-06-27 13:38:07 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+// Получаем канвас
+const canvas = document.getElementById('webgl-canvas');
+
+// Сцена
+const scene = new THREE.Scene();
+
+// Камера
+const camera = new THREE.PerspectiveCamera(
+  75, 
+  window.innerWidth / window.innerHeight, 
+  0.1, 
+  1000
+);
+camera.position.z = 2;
+
+// Рендерер
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+  alpha: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Ресайз под размер окна
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Анимация
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Пока ничего не рендерим кроме сцены
+  renderer.render(scene, camera);
+}
+
+animate();
 
